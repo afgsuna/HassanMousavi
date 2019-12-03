5fde35422d58f3211a88405da0dc4985c5a66740
afgsuna <afgsuna@yahoo.com>
2019-12-03 09:52:12 +0100

    Added sub method to the Match class

--- src/com/company/Math.java
+++ src/com/company/Math.java
@@ -4,4 +4,8 @@ public class Math {
     public int add(int first, int second){
         return first + second;
     }
+
+    public int sub(int first, int second){
+        return first - second;
+    }
 }
