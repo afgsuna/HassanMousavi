70872db135921059cd9d38e49e60e125989cd742
Gustav <neva.in.life@hotmail.com>
2019-12-03 10:13:04 +0100

    Changed Math sub

--- src/com/company/Math.java
+++ src/com/company/Math.java
@@ -1,11 +1,11 @@
 package com.company;
 
 public class Math {
-    public int add(int first, int second){
+    public int add(int first, int second)    {
         return first + second;
     }
 
     public int sub(int first, int second){
-        return first - second;
+        return first - second + 1243;
     }
 }
